4863a0c35ffa35bf3a4d49230018a6a61cd4052e
kmcknight1 <katiemcknight1991@gmail.com>
2019-10-07 20:16:05 -0500

    changes styling/component names

--- client/src/components/styled-comps/HTML5.js
+++ client/src/components/styled-comps/HTML5.js
@@ -4,14 +4,14 @@ import "../../styles/global.scss";
 
 import CodeData from "../CodeData";
 
-function NoStyling() {
+function HTML5() {
   return (
     <div className="Main">
       <NavLink className="link" to="/">
         Back to Home
       </NavLink>
-      <h1>No Styling</h1>
-      <div className="nostyling-page">
+      <h1>HTML5 Default</h1>
+      <div className="html5-page">
         <h2>Welcome Back!</h2>
         <h3>Login to ThisApp</h3>
         <br />
@@ -41,10 +41,9 @@ function NoStyling() {
         url={
           "https://raw.githubusercontent.com/kmcknight1/basic-styling/master/client/src/components/styled-comps/NoStyling.js"
         }
-        styles={false}
       />
     </div>
   );
 }
 
-export default NoStyling;
+export default HTML5;
